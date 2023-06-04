a9ae938a9b51520dece3d8ec78390e1a5922be93
Felipe <felipeallanf10@outlook.com>
2023-06-04 19:58:30 -0300

    o jogo funciona

--- js/game.js
+++ js/game.js
@@ -1,4 +1,8 @@
 const grid = document.querySelector('.grid')
+const spanPlayer = document.querySelector('.player')
+const timer = document.querySelector('.timer')
+const minuto = document.querySelector('#minuto')
+const dois_pontos = document.querySelector('#pontos')
 
 const characters = [
   'beth',
@@ -19,40 +23,110 @@ const createElement = (tag, className) => {
   return element
 }
 
-const revealCard = ({target}) =>{
+let firstCard = ''
+let secondCard = ''
+const checkEndGame = () => {
+  const disabledCards = document.querySelectorAll('.disabled-card')
 
-  if(target.parentNode.className.includes('reveal-card')){
-    return;
+  if (disabledCards.length === 20) {
+    clearInterval(this.loop)
+    clearInterval(this.looop)
+    setTimeout(() => {
+      alert(
+        `Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi de ${minuto.innerHTML}:${timer.innerHTML}`
+      )
+    }, 50)
+  }
+}
+const chekcards = () => {
+  const firstCharacter = firstCard.getAttribute('data-character')
+  const secondCharacter = secondCard.getAttribute('data-character')
+
+  if (firstCharacter === secondCharacter) {
+    setTimeout(() => {
+      firstCard.firstChild.classList.add('disabled-card')
+      secondCard.firstChild.classList.add('disabled-card')
+      firstCard = ''
+      secondCard = ''
+
+      checkEndGame()
+    }, 600)
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove('reveal-card')
+      secondCard.classList.remove('reveal-card')
+
+      firstCard = ''
+      secondCard = ''
+    }, 600)
+  }
+}
+const revealCard = ({ target }) => {
+  if (target.parentNode.className.includes('reveal-card')) {
+    return
   }
 
-  target.parentNode.classList.add('reveal-card')
+  if (firstCard === '') {
+    target.parentNode.classList.add('reveal-card')
+    firstCard = target.parentNode
+  } else if (secondCard === '') {
+    target.parentNode.classList.add('reveal-card')
+    secondCard = target.parentNode
+    chekcards()
+  }
 }
 
-const createCard = (character) => {
+const createCard = character => {
   const card = createElement('div', 'card')
   const front = createElement('div', 'face front')
   const back = createElement('div', 'face back')
 
-  front.style.backgroundImage = `url('../images/${character}.png')`;
+  front.style.backgroundImage = `url('../images/${character}.png')`
 
   card.appendChild(front)
   card.appendChild(back)
 
-  card.addEventListener('click', revealCard);
+  card.addEventListener('click', revealCard)
+  card.setAttribute('data-character', character)
   return card
 }
 
 const loadGame = () => {
+  const duplicateCharacters = [...characters, ...characters]
+  const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5)
 
-  const duplicateCharacters = [...characters, ...characters];
-  const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
-
-  shuffledArray.forEach((character) => {
-    
-    const card = createCard(character);
-    grid.appendChild(card);
-
-  });
-
+  shuffledArray.forEach(character => {
+    const card = createCard(character)
+    grid.appendChild(card)
+  })
 }
-loadGame()
+
+const startTimer = () => {
+  this.loop = setInterval(() => {
+    let currentTime = Number(timer.innerHTML)
+    if (currentTime < 60) {
+      timer.innerHTML = currentTime + 1
+    } else {
+      currentTime = 0
+      timer.innerHTML = currentTime + 1
+    }
+  }, 1000)
+
+  if(minuto.innerHTML == 0){
+    minuto.style.display = 'none'
+  }
+  this.looop = setInterval(() => {
+    minuto.style.display = 'flex'
+    let currentMinute = Number(minuto.innerHTML)
+    minuto.innerHTML = currentMinute + 1
+    if(minuto.innerHTML > 0){
+      dois_pontos.style.display = 'flex';
+    }
+  }, 60000)
+}
+
+window.onload = () => {
+  spanPlayer.innerHTML = localStorage.getItem('player')
+  startTimer()
+  loadGame()
+}
